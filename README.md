069124a609456dd4dffa7c523fcdb70fc078cbc3
nuryani21 <nuryaniishak27@gmail.com>
2026-05-24 16:56:18 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # FoundU
+
+FoundU is a campus lost and found management web application.
+
+## Features
+
+- Authentication Login/Register
+- Dashboard Summary
+- Data Visualisation Chart
+- Found Items Page
+- Reports Management
+- Notifications
+- Responsive Design
+
+## Framework
+
+- Tailwind CSS
+- Chart.js
+- JavaScript
+
+## Login Test
+
+Email:
+admin@foundu.com
+
+Password:
+123456
---
+++ script.js
@@ -0,0 +1,20 @@
+function login(){
+
+    let username =
+    document.getElementById("username").value;
+
+    let password =
+    document.getElementById("password").value;
+
+    if(username === "admin" && password === "1234"){
+
+        window.location.href = "dashboard.html";
+
+    }else{
+
+        document.getElementById("error_message").innerHTML =
+        "Invalid username or password";
+
+    }
+
+}
